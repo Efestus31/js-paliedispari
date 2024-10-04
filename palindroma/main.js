8e914cb274fe8b8f1a2eb3e044bd35d3a9a5c808
Efestus31 <biagiohd@gmail.com>
2024-10-04 15:49:52 +0200

    fine esercizio + inizio piccolo bonus personale

--- palindroma/main.js
+++ palindroma/main.js
@@ -4,7 +4,7 @@ console.log('test');
 /**
  * 0. strumenti da utilizzare
  *      function   
- *      if/els if/else
+ *      if/else 
  *      prompt
  *      split
  *      reverse
@@ -14,6 +14,30 @@ console.log('test');
  */
 
 //prompt dall' utente, variabile bool posta a falso
-let pali_to_check = ("Ciao!, inserisci una parola che secondo te è un palindroma!");
-let check = false;
+let pali_to_check = prompt("Ciao!, inserisci una parola che secondo te è un palindroma!");
+let reite = false;
+
+//function per invertire l'input dato dall'utente
+
+function string_reverse(pal){
+    //usiamo split per trasformare la stringa in array
+    let split_string = pal.split("");
+
+    //usiamo reverse per invertire l'array appena creato   
+    let array_inverso = split_string.reverse();
+
+    //usiamo join per fonderlo di nuovo insieme
+    let reverse_string = array_inverso.join("");
+    
+    //return con stringa invertita e verifico se la parola è palindroma
+    return reverse_string === pali_to_check;
+}
+
+if(string_reverse(pali_to_check)){
+    console.log(`${pali_to_check} è palindroma`);
+    
+} else{
+    console.log(`${pali_to_check} non è palindroma`);
+    
+}
 
