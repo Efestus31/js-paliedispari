98c9c1564a1cfaebe9fee79b1d8d109f9a885a35
Efestus31 <biagiohd@gmail.com>
2024-10-04 15:12:05 +0200

    variables

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,1 +1,19 @@
 console.log('test');
+
+
+/**
+ * 0. strumenti da utilizzare
+ *      function   
+ *      if/els if/else
+ *      prompt
+ *      split
+ *      reverse
+ *      join
+ *      let/const
+ *      bool
+ */
+
+//prompt dall' utente, variabile bool posta a falso
+let pali_to_check = ("Ciao!, inserisci una parola che secondo te è un palindroma!");
+let check = false;
+
